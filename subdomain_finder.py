e8247114bed9e8afa83c0585ba3ea05159711f66
hanniux <67535034+hanniux@users.noreply.github.com>
2023-07-14 22:37:46 +0630

    Create subdomain_finder.py

---
+++ subdomain_finder.py
@@ -0,0 +1,16 @@
+import requests
+
+bright_red = "\033[0;91m"
+
+domain = input("Enter Domain Name: ")
+file = open("list.txt")
+content = file.read()
+subdomains = content.splitlines()
+for subdomain in subdomains:
+    url = f"http://{subdomain}.{domain}"
+    try:
+        requests.get(url)
+    except requests.ConnectionError:
+        pass
+    else:
+        print(bright_red + "[+] Discovered subdomain:", url)
